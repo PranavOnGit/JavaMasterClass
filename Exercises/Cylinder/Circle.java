e66dbf1a83872976995adcd3e2af4f4bf4a03311
Pranav Chavan <tasksontable@gmail.com>
2020-08-01 08:55:31 +0530

    Inhertance Exercises

---
+++ Exercises/Cylinder/Circle.java
@@ -0,0 +1,21 @@
+package Exercises.Cylinder;
+
+//Please check out Cylinder.txt for problem statement.
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        if(radius < 0){
+            this.radius =0;
+        }else{
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        return this.radius * this.radius * Math.PI;
+    }
+}
---
+++ Exercises/PoolArea/Cuboid.java
@@ -0,0 +1,23 @@
+package Exercises.PoolArea;
+
+//Please refer Rectangle.txt for problem statement
+public class Cuboid extends Rectangle{
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+
+        if(height < 0){
+            height = 0;
+        }else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume(){
+        return getArea() * this.height;
+    }
+}

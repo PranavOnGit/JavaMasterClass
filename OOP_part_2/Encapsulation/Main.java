dec84a3eec99dd173636d85b28f7965686f9c7f7
Pranav Chavan <tasksontable@gmail.com>
2020-08-06 20:27:45 +0530

    composition

---
+++ OOP_part_2/Encapsulation/Main.java
@@ -0,0 +1,13 @@
+package OOP_part_2.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Printer printer = new Printer(56, false);
+
+        System.out.println("Initial page count: "+ printer.getNumOfPagesPrinted());
+        int pagesPrinted =  printer.printPages(4);
+        System.out.println("Pages printed was "+ pagesPrinted +", new total print count for printer is "+ printer.getNumOfPagesPrinted());
+        pagesPrinted =  printer.printPages(2);
+        System.out.println("Pages printed was "+ pagesPrinted +", new total print count for printer is "+ printer.getNumOfPagesPrinted());
+    }
+}
---
+++ OOP_part_2/Encapsulation/Printer.java
@@ -0,0 +1,53 @@
+package OOP_part_2.Encapsulation;
+
+// Create a class and demonstrate proper encapsulation techniques
+// the class will be called Printer
+// It will simulate a real Computer Printer
+// It should have fields for the toner Level, number of pages printed, and
+// also whether its a duplex printer (capable of printing on both sides of the paper).
+// Add methods to fill up the toner (up to a maximum of 100%), another method to
+// simulate printing a page (which should increase the number of pages printed).
+// Decide on the scope, whether to use constructors, and anything else you think is needed.
+
+public class Printer {
+    private int toneLevel;
+    private int numOfPagesPrinted;
+    private boolean duplex;
+
+    //Constructor
+    public Printer(int toneLevel, boolean duplex) {
+        if(toneLevel > -1 && toneLevel <= 100){
+            this.toneLevel = toneLevel;
+        }else {
+            this.toneLevel = -1;
+        }
+        this.numOfPagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount){
+        if (tonerAmount >0 && tonerAmount <=100 ){
+            if (this.toneLevel + tonerAmount > 100){
+                return -1;
+            }
+            this.toneLevel += tonerAmount;
+            return this.toneLevel;
+        }else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages){
+        int pagesToPrint = pages;
+        if (duplex){
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode!");
+        }
+        this.numOfPagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getNumOfPagesPrinted() {
+        return numOfPagesPrinted;
+    }
+}
